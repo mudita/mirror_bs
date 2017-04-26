7af9dffba13c67824617e5d9f1bd4008847ffce3
Tomasz Orłowski <torlowsk@gmail.com>
2017-04-26 16:45:00 +0200

    Added self_test.log generator.

--- self_test.mk
+++ self_test.mk
@@ -21,12 +21,12 @@ SELF_TEST_APPLICATIONS_RULE=		$(CONFIG_SELF_TEST_FILE_NAME)_$(SELF_TEST_APPLICAT
 SELF_TEST_MODULES_RULE=			$(CONFIG_SELF_TEST_FILE_NAME)_$(SELF_TEST_MODULES_SUFFIX)
 
 SELF_TEST_LOOP_FIRST_FORMATER=		' %s\n\t\t%s_%s\n\n'
-SELF_TEST_LOOP_FORMATER=		'%s_%s: %s\n\t\t%s\n\t%s\n\t\t%s\n\t%s\n\t\t%s\n\n'
+SELF_TEST_LOOP_FORMATER=		'%s_%s: %s\n\t\t%s\n\t%s\n\t\t%s\n\t%s\n\t\t%s\n\t%s\n\t\t%s\n\t\t%s\n\n'
 SELF_TEST_LOOP_THIRD_FORMATER=		'%s_%s:'
 SELF_TEST_RULE_FORMATER=		'%s\n\t\t%s\n\n'
 SELF_TEST_BASE_FORMATER=		'%s'
-SELF_TEST_EOF_MODULES_FORMATER=		' \\\n\t\t%s\n\n'
-SELF_TEST_EOF_APPLICATIONS_FORMATER=	'\n\n'
+SELF_TEST_EOF_APPLICATIONS_FORMATER=	' \\\n\t\t%s\n\n'
+SELF_TEST_EOF_MODULES_FORMATER=		'\n\n'
 
 SELF_TEST_FILE_NAME=			$(CONFIG_SELF_TEST_FILE_NAME).$(CONFIG_BUILD_SYSTEM_SCRIPT_EXT)
 SELF_TEST_APPLICATIONS_FILE_NAME=	$(SELF_TEST_APPLICATIONS_RULE).$(CONFIG_BUILD_SYSTEM_SCRIPT_EXT)
@@ -50,54 +50,6 @@ $(SELF_TEST_FILE): \
 	printf \
 		$(SELF_TEST_RULE_FORMATER) \
 		'$$(SELF_TEST_RULE): \' \
-		'$$(SELF_TEST_MODULES_RULE)' \
-		>> \
-		$*
-
-$(SELF_TEST_MODULES_FILE): \
-		%: \
-		$(DIRS_TEMP_DIR)
-	cp \
-		/dev/null \
-		$*
-	printf \
-		$(SELF_TEST_BASE_FORMATER) \
-		'$$(SELF_TEST_MODULES_RULE):' \
-		>> \
-		$*
-	for \
-		module \
-		in \
-		$(MODULES_PLATFORMS_LIST); \
-		do \
-			printf \
-				$(SELF_TEST_LOOP_FIRST_FORMATER) \
-				'\' \
-				'$$(SELF_TEST_PREFIX)' \
-				$$module \
-				>> \
-				$*; \
-			printf \
-				$(SELF_TEST_LOOP_FORMATER) \
-				'$$(SELF_TEST_PREFIX)' \
-				$$module \
-				'\' \
-				'$$(SELF_TEST_PREFIX)_%:' \
-				'make \' \
-				'$(CONFIG_CLEAN_RULE)' \
-				'make \' \
-				'$$*' \
-				>> \
-				$*; \
-			printf \
-				$(SELF_TEST_LOOP_THIRD_FORMATER) \
-				'$$(SELF_TEST_PREFIX)' \
-				$$module \
-				>> \
-				$*; \
-		done
-	printf \
-		$(SELF_TEST_EOF_MODULES_FORMATER) \
 		'$$(SELF_TEST_APPLICATIONS_RULE)' \
 		>> \
 		$*
@@ -132,9 +84,12 @@ $(SELF_TEST_APPLICATIONS_FILE): \
 				'\' \
 				'$$(SELF_TEST_PREFIX)_%:' \
 				'make \' \
-				'$(CONFIG_CLEAN_RULE)' \
+				'$$(CONFIG_CLEAN_RULE)' \
 				'make \' \
 				'$$*' \
+				'echo \' \
+				'$$* >> \' \
+				'self_test.log' \
 				>> \
 				$*; \
 			printf \
@@ -146,6 +101,57 @@ $(SELF_TEST_APPLICATIONS_FILE): \
 		done
 	printf \
 		$(SELF_TEST_EOF_APPLICATIONS_FORMATER) \
+		'$$(SELF_TEST_MODULES_RULE)' \
+		>> \
+		$*
+
+$(SELF_TEST_MODULES_FILE): \
+		%: \
+		$(DIRS_TEMP_DIR)
+	cp \
+		/dev/null \
+		$*
+	printf \
+		$(SELF_TEST_BASE_FORMATER) \
+		'$$(SELF_TEST_MODULES_RULE):' \
+		>> \
+		$*
+	for \
+		module \
+		in \
+		$(MODULES_PLATFORMS_LIST); \
+		do \
+			printf \
+				$(SELF_TEST_LOOP_FIRST_FORMATER) \
+				'\' \
+				'$$(SELF_TEST_PREFIX)' \
+				$$module \
+				>> \
+				$*; \
+			printf \
+				$(SELF_TEST_LOOP_FORMATER) \
+				'$$(SELF_TEST_PREFIX)' \
+				$$module \
+				'\' \
+				'$$(SELF_TEST_PREFIX)_%:' \
+				'make \' \
+				'$$(CONFIG_CLEAN_RULE)' \
+				'make \' \
+				'$$*' \
+				'echo \' \
+				'$$* >> \' \
+				'self_test.log' \
+				>> \
+				$*; \
+			printf \
+				$(SELF_TEST_LOOP_THIRD_FORMATER) \
+				'$$(SELF_TEST_PREFIX)' \
+				$$module \
+				>> \
+				$*; \
+		done
+	printf \
+		$(SELF_TEST_EOF_MODULES_FORMATER) \
 		>> \
 		$*
 
