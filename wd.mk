d981cde43a72773e63f6153ae8e37597bf3a310d
Tomasz Orłowski <torlowsk@gmail.com>
2017-04-27 22:33:46 +0200

    Simplified build system script module.

--- wd.mk
+++ wd.mk
@@ -1,10 +1,19 @@
 ifndef MK_WD_MK
 MK_WD_MK=			TRUE
 
+INCLUDER_MODULES_LIST=		dirs \
+				platform
+
+ifndef INCLUDER_PATH
+$(error tool modbuild is not installed in your build system!)
+else
+include $(INCLUDER_PATH)
+endif
+
 ifdef WD
 WD_DIR=				$(WD)
 else
-WD_DIR=				$(DIRS_INSTALL_DIR)
+WD_DIR=				$(DIRS_INSTALL_DIR)/$(PLATFORM)
 endif
 
 endif
