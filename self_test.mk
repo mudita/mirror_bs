67be8f99b9633567f14e64f9a79d459557f5465d
Tomasz Orłowski <torlowsk@gmail.com>
2017-05-04 18:06:24 +0200

    Added tools testing. Changed order of tests performed during self test procedure.

--- self_test.mk
+++ self_test.mk
@@ -14,11 +14,13 @@ endif
 
 SELF_TEST_APPLICATIONS_SUFFIX=		$(CONFIG_APPLICATION_PREFIX)s
 SELF_TEST_MODULES_SUFFIX=		$(CONFIG_MODULE_PREFIX)s
+SELF_TEST_TOOLS_SUFFIX=		$(CONFIG_TOOL_PREFIX)s
 
 SELF_TEST_PREFIX=			$(CONFIG_SELF_TEST_FILE_NAME)
 SELF_TEST_RULE=				$(CONFIG_SELF_TEST_FILE_NAME)
 SELF_TEST_APPLICATIONS_RULE=		$(CONFIG_SELF_TEST_FILE_NAME)_$(SELF_TEST_APPLICATIONS_SUFFIX)
 SELF_TEST_MODULES_RULE=			$(CONFIG_SELF_TEST_FILE_NAME)_$(SELF_TEST_MODULES_SUFFIX)
+SELF_TEST_TOOLS_RULE=			$(CONFIG_SELF_TEST_FILE_NAME)_$(SELF_TEST_TOOLS_SUFFIX)
 
 SELF_TEST_LOOP_FIRST_FORMATER=		\ %s\\n\\t\\t%s_%s\\n\\n
 SELF_TEST_LOOP_SECOND_FORMATER=		%s_%s:\ \\\\\\n\\t\\t%s\\n
@@ -33,7 +35,9 @@ SELF_TEST_RULE_SECOND_FORMATER=		\\t%s\\n\\t\\t%s\\n\\n
 SELF_TEST_BASE_FORMATER=		%s
 
 SELF_TEST_EOF_APPLICATIONS_FORMATER=	\ \\\\\\n\\t\\t%s\\n\\n
-SELF_TEST_EOF_MODULES_FORMATER=		\\n\\n
+#SELF_TEST_EOF_MODULES_FORMATER=		\\n\\n
+SELF_TEST_EOF_MODULES_FORMATER=		\ \\\\\\n\\t\\t%s\\n\\n
+SELF_TEST_EOF_TOOLS_FORMATER=		\\n\\n
 
 SELF_TEST_GREP_MATCH_PATTERN=		\^\\t''\\t$$*
 SELF_TEST_GREP_FLAGS=			-m \
@@ -43,10 +47,12 @@ SELF_TEST_GREP_FLAGS=			-m \
 SELF_TEST_FILE_NAME=			$(CONFIG_SELF_TEST_FILE_NAME).$(CONFIG_BUILD_SYSTEM_SCRIPT_EXT)
 SELF_TEST_APPLICATIONS_FILE_NAME=	$(SELF_TEST_APPLICATIONS_RULE).$(CONFIG_BUILD_SYSTEM_SCRIPT_EXT)
 SELF_TEST_MODULES_FILE_NAME=		$(SELF_TEST_MODULES_RULE).$(CONFIG_BUILD_SYSTEM_SCRIPT_EXT)
+SELF_TEST_TOOLS_FILE_NAME=		$(SELF_TEST_TOOLS_RULE).$(CONFIG_BUILD_SYSTEM_SCRIPT_EXT)
 
 SELF_TEST_FILE=				$(DIRS_BS_TEMP_DIR)/$(SELF_TEST_FILE_NAME)
 SELF_TEST_APPLICATIONS_FILE=		$(DIRS_BS_TEMP_DIR)/$(SELF_TEST_APPLICATIONS_FILE_NAME)
 SELF_TEST_MODULES_FILE=			$(DIRS_BS_TEMP_DIR)/$(SELF_TEST_MODULES_FILE_NAME)
+SELF_TEST_TOOLS_FILE=			$(DIRS_BS_TEMP_DIR)/$(SELF_TEST_TOOLS_FILE_NAME)
 
 ifneq ($(wildcard $(SELF_TEST_FILE)), )
 $(CONFIG_ALL_RULE): \
@@ -138,7 +144,7 @@ $(SELF_TEST_APPLICATIONS_FILE): \
 
 $(SELF_TEST_MODULES_FILE): \
 		%: \
-		$(DIRS_BS_TEMP_DIR)
+		$(SELF_TEST_TOOLS_FILE)
 	cp \
 		/dev/null \
 		$*
@@ -167,6 +173,13 @@ $(SELF_TEST_MODULES_FILE): \
 				>> \
 				$*; \
 			printf \
+				$(SELF_TEST_LOOP_CONDITION_FORMATER) \
+				'if [ -z "$$(shell grep $(SELF_TEST_GREP_FLAGS) $(SELF_TEST_GREP_MATCH_PATTERN) $(DEPENDENCIES_FILE) | tr -d \\\)" ]; then \' \
+				'echo $$* never used; false; \' \
+				'fi' \
+				>> \
+				$*; \
+			printf \
 				$(SELF_TEST_LOOP_THIRD_FORMATER) \
 				'make \' \
 				'$$(CONFIG_CLEAN_RULE)' \
@@ -176,13 +189,6 @@ $(SELF_TEST_MODULES_FILE): \
 				$(SELF_TEST_LOOP_THIRD_FORMATER) \
 				'make \' \
 				'$$*' \
-				>> \
-				$*; \
-			printf \
-				$(SELF_TEST_LOOP_CONDITION_FORMATER) \
-				'if [ -z "$$(shell grep $(SELF_TEST_GREP_FLAGS) $(SELF_TEST_GREP_MATCH_PATTERN) $(DEPENDENCIES_FILE) | tr -d \\\)" ]; then \' \
-				'echo $$* never used; false; \' \
-				'fi' \
 				>> \
 				$*; \
 			printf \
@@ -201,6 +207,76 @@ $(SELF_TEST_MODULES_FILE): \
 		done
 	printf \
 		$(SELF_TEST_EOF_MODULES_FORMATER) \
+		'$$(SELF_TEST_TOOLS_RULE)' \
+		>> \
+		$*
+
+
+$(SELF_TEST_TOOLS_FILE): \
+		%: \
+		$(DIRS_BS_TEMP_DIR)
+	cp \
+		/dev/null \
+		$*
+	printf \
+		$(SELF_TEST_BASE_FORMATER) \
+		'$$(SELF_TEST_TOOLS_RULE):' \
+		>> \
+		$*
+	for \
+		$(CONFIG_TOOL_PREFIX) \
+		in \
+		$(TOOLS_PLATFORMS_LIST); \
+		do \
+			printf \
+				$(SELF_TEST_LOOP_FIRST_FORMATER) \
+				'\' \
+				'$$(SELF_TEST_PREFIX)' \
+				$$$(CONFIG_TOOL_PREFIX) \
+				>> \
+				$*; \
+			printf \
+				$(SELF_TEST_LOOP_SECOND_FORMATER) \
+				'$$(SELF_TEST_PREFIX)' \
+				$$$(CONFIG_TOOL_PREFIX) \
+				'$$(SELF_TEST_PREFIX)_%:' \
+				>> \
+				$*; \
+			printf \
+				$(SELF_TEST_LOOP_CONDITION_FORMATER) \
+				'if [ -z "$$(shell grep $(SELF_TEST_GREP_FLAGS) $(SELF_TEST_GREP_MATCH_PATTERN) $(DEPENDENCIES_FILE) | tr -d \\\)" ]; then \' \
+				'echo $$* never used; false; \' \
+				'fi' \
+				>> \
+				$*; \
+			printf \
+				$(SELF_TEST_LOOP_THIRD_FORMATER) \
+				'make \' \
+				'$$(CONFIG_CLEAN_RULE)' \
+				>> \
+				$*; \
+			printf \
+				$(SELF_TEST_LOOP_THIRD_FORMATER) \
+				'make \' \
+				'$$*' \
+				>> \
+				$*; \
+			printf \
+				$(SELF_TEST_LOOP_LOG_FORMATER) \
+				'echo \' \
+				'$$* >> \' \
+				'self_test.log' \
+				>> \
+				$*; \
+			printf \
+				$(SELF_TEST_LOOP_LAST_FORMATER) \
+				'$$(SELF_TEST_PREFIX)' \
+				$$$(CONFIG_TOOL_PREFIX) \
+				>> \
+				$*; \
+		done
+	printf \
+		$(SELF_TEST_EOF_TOOLS_FORMATER) \
 		>> \
 		$*
 
@@ -214,6 +290,10 @@ include $(SELF_TEST_APPLICATIONS_FILE)
 
 #ifneq ($(wildcard $(SELF_TEST_MODULES_FILE)), )
 include $(SELF_TEST_MODULES_FILE)
+#endif
+
+#ifneq ($(wildcard $(SELF_TEST_TOOLS_FILE)), )
+include $(SELF_TEST_TOOLS_FILE)
 #endif
 
 endif
