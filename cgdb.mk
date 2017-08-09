f37951741f72005c2a6d640c128b4a8f16a041de
Tomasz Orłowski <torlowsk@gmail.com>
2017-08-09 17:21:03 +0200

    Updated build environment.

--- cgdb.mk
+++ cgdb.mk
@@ -45,7 +45,7 @@ CGDB_GDB_COMMAND=		target\ \
 CGDB_FLAGS=			-d \
 				$(PLATFORM_GDB) \
 				-x \
-				$(DIRS_GDB_DIR)/$(CONFIG_GDB_SCRIPT_FILE_NAME) \
+				$(DIRS_GDB_DIR)/$(PLATFORM).$(CONFIG_GDB_SCRIPT_EXT) \
 				-ex \
 				$(CGDB_GDB_COMMAND) \
 
