7c03dc006d38ea768ab7ce61186ac10efadbbded
Tomasz Orłowski <torlowsk@gmail.com>
2017-08-11 01:11:54 +0200

    Working on enable cortex m4.

--- flags_compiler/optimization.mk
+++ flags_compiler/optimization.mk
@@ -2225,8 +2225,11 @@ FLAGS_COMPILER_OPTIMIZATION=	-O0
 #       so. When you specify these options, the assembler and linker create
 #       larger object and executable files and are also slower. You cannot use
 #       gprof on all systems if you specify this option, and you may have
-#       problems with debugging if you specify both this option and -g. 
-#
+#       problems with debugging if you specify both this option and -g.
+
+FLAGS_COMPILER_OPTIMIZATION+=	-ffunction-sections
+FLAGS_COMPILER_OPTIMIZATION+=	-fdata-sections
+
 # INFO: -fbranch-target-load-optimize
 #       Perform branch target register load optimization before prologue /
 #       epilogue threading. The use of target registers can typically be
