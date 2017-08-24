95af70cfa394e2cd74955ff31dd3a7296dd36923
Tomasz Orłowski <torlowsk@gmail.com>
2017-08-24 16:26:20 +0200

    Enabled using external defines from file.

--- defines.mk
+++ defines.mk
@@ -15,24 +15,38 @@ endif
 DEFINES_PREFIX=			-D
 
 # INFO: Do not use quotations ("), use apostrophes instead (').
-#DEFINES_BARE_LIST=		BARE_DEFINE_NAME='bare_define_value'
+#DEFINES_DEFAULT_LIST=		BARE_DEFINE_NAME='bare_define_value'
 
-# TODO: Add defines from files with metaprograming ability.
-# TODO: Implement PATH variable in smart way.
-DEFINES_BARE_LIST=		ETRACE_IF_CHRONOGRAPH_COMMAND='chronograph_gcc_7_1_1_x86_64_pc_linux_gnu' \
+# TODO: Add defines from files with metaprograming ability (makefile variables
+#       enabled in defines file). Currently compiler version cannot be
+#       generated or taken from build system script.
+DEFINES_DEFAULT_LIST=		ETRACE_IF_CHRONOGRAPH_COMMAND='chronograph_gcc_7_1_1_x86_64_pc_linux_gnu' \
 				CPU_LOGS_CHRONOGRAPH_COMMAND='chronograph_gcc_7_1_1_x86_64_pc_linux_gnu' \
 				USE_STDPERIPH_DRIVER \
 				OS_USE_SEMIHOSTING
 
 ifeq ($(UNIT_TEST), 1)
-DEFINES_BARE_LIST+=		UNIT_TEST_ENTRY_POINT=$(UNIT_TEST_ENTRY_POINT)
+DEFINES_DEFAULT_LIST+=		UNIT_TEST_ENTRY_POINT=$(UNIT_TEST_ENTRY_POINT)
+endif
+
+DEFINES_SPECYFIC_EXISTENCE=	$(wildcard \
+					$(CONFIG_DEFINES_FILE_NAME))
+
+ifneq ($(DEFINES_SPECYFIC_EXISTENCE), )
+DEFINES_SPECYFIC_LIST_COMMAND=	cat \
+					$(CONFIG_DEFINES_FILE_NAME)
+
+DEFINES_SPECYFIC_LIST=		$(shell \
+					$(DEFINES_SPECYFIC_LIST_COMMAND))
+
+DEFINES_DEFAULT_LIST+=		$(DEFINES_SPECYFIC_LIST)
 endif
 
 # INFO: Each bare define gets define prefix here.
 DEFINES_LIST=			$(patsubst \
 					%, \
 					$(DEFINES_PREFIX) %, \
-					$(DEFINES_BARE_LIST))
+					$(DEFINES_DEFAULT_LIST))
 
 endif
 
