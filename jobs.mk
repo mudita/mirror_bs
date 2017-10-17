6a0bd8b6013859b354cab0793f59343842fc70e3
Tomasz Orłowski <torlowsk@gmail.com>
2017-10-17 15:24:56 +0200

    Working libwebsockets fix.

--- jobs.mk
+++ jobs.mk
@@ -8,14 +8,8 @@ JOBS_COUNT_PROCESSORS_COMMAND=	cat $(JOBS_CPU_INFO) | \
 					grep $(JOBS_CPU_PATTERN) | \
 					wc -l
 
-JOBS_PROCESSORS_COUNT=		$(shell \
-					$(JOBS_COUNT_PROCESSORS_COMMAND))
-
-JOBS_COUNT_COMMAND=		echo \
-					$$(( ($(JOBS_PROCESSORS_COUNT) * 2) + 1 ))
-
 JOBS_COUNT=			$(shell \
-					$(JOBS_COUNT_COMMAND))
+					$(JOBS_COUNT_PROCESSORS_COMMAND))
 
 endif
 
